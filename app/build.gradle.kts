9d2eae466d0824518ff1d5d6b0bc5ed1dd501524
valborgs <voll1212@naver.com>
2025-12-19 22:12:57 +0900

    chore: 버전 업데이트 및 README 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "org.comon.pdfredactorm"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.1.0"
+        versionCode = 13
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
